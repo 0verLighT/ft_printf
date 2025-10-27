21aa28f7d08a3f992c37ed721efc2910a87bdfe8
0verLighT <alexandre@0verlight.com>
2025-10-27 21:55:59 +0100

    chore: based function

---
+++ ft_putchar.c
@@ -0,0 +1,6 @@
+#include "include/ft_prinft.h"
+
+void	ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
---
+++ ft_putstr.c
@@ -0,0 +1,10 @@
+#include "include/ft_printf.h"
+
+void    ft_putstr(char *s)
+{
+    while (*s)
+    {
+        write(1, s, 1);
+        s++;
+    }
+}
